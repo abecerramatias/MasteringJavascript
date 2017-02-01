b4253e9a0e55e257ea49789134910acd82a98079
Alberto Becerra Matias <abecerramatias@gmail.com>
2017-02-01 18:33:03 -0400

    last changes for cross browser javascript

--- Cross-browser-Development/js/script.js
+++ Cross-browser-Development/js/script.js
@@ -4,53 +4,46 @@
 
 var q;
 
-
 window.onload=function(){
-	console.log("window is loaded.");
+	console.log("window is loaded");
 }
 
-
 addOnReady(function(){
-	console.log("2 window is loaded.");
+	console.log("2 window is loaded");
 });
 
 addOnReady(function(){
-	console.log("3 window is loaded.");
+	console.log("3 window is loaded");
 
 	if(document.querySelectorAll && document.querySelectorAll('body:first-of-type')){
 		q = function(parm) {
 			return document.querySelectorAll(parm);
 		};
-		
-		onReadySelect();
-
+        onReadySelect();
 	}else{
 		loadScript('js/sizzle.min.js', function(){
 			q = Sizzle;
-			onReadySelect();
+            onReadySelect();
 		});
 	}
-
 });
 
-function onReadySelect(){
-	var span = q('#msg span')[0],
-			innerText = (span.innerText===undefined) ? 'textContent':'innerText';
+function onReadySelect() {
+    var span = q('#msg span')[0],
+        innerText = (span.innerText===undefined) ? 'textContent' : 'innerText';
 
-	span[innerText] = 'Yay!! We Made It Mom!!!';
+    span[innerText] = 'We made it!!';
 }
 
 addOnReady(function(){
-	console.log("4 window is loaded.");
+	console.log("4 window is loaded");
 
 	if(false && Modernizr.audio.mp3){
 		var snd = new Audio();
-				snd.src = "sample.mp3";
-				snd.play();
+			snd.src = "sample.mp3";
+			snd.play();
 	}
-
 });
-
 
 function addOnReady(fun){
 	var last = window.onload;
@@ -69,33 +62,22 @@ function addOnReady(fun){
 
 		if(isReady) fun();
 	}
-
 }
-
 
 function loadScript(path, callback){
 	var js = document.createElement('script');
-			js.src = path;
-			js.type = 'text/javascript';
-			js.onload = function(){
-				callback();
-				this.onload = this.onreadystatechange = null;
-			};
+		js.src = path;
+		js.type = 'text/javascript';
+		js.onload = function(){
+			callback();
+			this.onload = this.onreadystatechange = null;
+		};
 
-			js.onreadystatechange = function(){
-				if(this.readState == 'complete'){
-					this.onload();
-				}
-			}
+	js.onreadystatechange = function(){
+		if(this.readState == 'complete'){
+			this.onload();
+		}
+	}
 
-			document.getElementsByTagName('head')[0].appendChild(js);
+	document.getElementsByTagName('head')[0].appendChild(js);
 }
-
-
-
-
-
-
-
-
-
